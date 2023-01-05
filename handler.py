1549229cad7e85ce5ff77ca79127b0cfb9d26dff
Icemap <qizhi.wang@pingcap.com>
2023-01-05 19:32:40 +0800

    [Update] Trigger the CI/CD

--- handler.py
+++ handler.py
@@ -3,19 +3,10 @@ import json
 
 def hello(event, context):
     body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
+        "message": "This is an example for serverless framework",
         "input": event,
     }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
